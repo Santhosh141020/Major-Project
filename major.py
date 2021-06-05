4c58da95835f6b25f65bec620775b063db506586
D Santhosh Murthy <77673685+Santhosh141020@users.noreply.github.com>
2021-06-05 17:29:34 +0530

    a lot of changes compared to previous one

--- major.py
+++ major.py
@@ -6,11 +6,12 @@ from emot.emo_unicode import UNICODE_EMO
 import contractions
 import nltk
 from nltk.tokenize.toktok import ToktokTokenizer
+
 nltk.download('stopwords')
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import streamlit as st
 
-
+@st.cache
 def get_urls(name):
     links = []
     products = []
@@ -18,7 +19,7 @@ def get_urls(name):
     dic = {}
 
     if name == '':
-        return(dic)
+        return (dic)
 
     try:
         name = name.replace(' ', '%20')
@@ -52,7 +53,7 @@ def get_urls(name):
 
     else:
         brands = []
-        models=[]
+        models = []
         for link in soup.findAll("a", attrs={"class": "IRpwTa"}):
             n_link = link.get('href')
             models.append(link.get_text())
@@ -64,10 +65,10 @@ def get_urls(name):
             brands.append(product.get_text())
         brands = brands[:20]
         models = models[:20]
-        for i in range(len(brand)):
-          products.append(brand[i]+ ' '+model[i])
+        for i in range(len(brands)):
+            products.append(brands[i] + ' ' + models[i])
         links = links[:20]
-        img_links = img_links[:20] 
+        img_links = img_links[:20]
 
     for i in range(len(links)):
         links[i] = "https://www.flipkart.com" + links[i]
@@ -76,12 +77,13 @@ def get_urls(name):
         products[i] = products[i].replace('-', '')
         products[i] = products[i].replace('  ', '')
         if len(products[i]) > 50:
-            products[i]=products[i][:50]
+            products[i] = products[i][:50]
         dic[products[i]] = [links[i], img_links[i]]
 
     return dic
 
-def get_reviews( dic, new):
+
+def get_reviews(dic, new):
     reviews = []
     url = dic[new][0]
 
@@ -92,24 +94,28 @@ def get_reviews( dic, new):
         reviews.append(r.get_text())
     return reviews
 
+
 # Remove HTML Tag
 def html_tag(text):
-    Soup = BeautifulSoup(text,"html.parser")
+    Soup = BeautifulSoup(text, "html.parser")
     new_text = Soup.get_text()
     return new_text
 
+
 # Expand Contractions
 def con(text):
-  expand= contractions.fix(text)
-  return expand
+    expand = contractions.fix(text)
+    return expand
 
-#Remove Special Characters
+
+# Remove Special Characters
 def remove_sp(text):
-  pattern = r'[^A-Za-z0-9\s]'
-  text = re.sub(pattern,'',text)
-  return text
+    pattern = r'[^A-Za-z0-9\s]'
+    text = re.sub(pattern, '', text)
+    return text
 
-#Remove stopwords
+
+# Remove stopwords
 def remove_stopwords(text):
     # creating a stopword list
     stopword_list = nltk.corpus.stopwords.words('english')
@@ -127,13 +133,15 @@ def remove_stopwords(text):
     filtered_text = ' '.join(filtered_tokens)
     return filtered_text
 
+
 # convert emojis to words
 def convert_emojis(text):
-  for emot in UNICODE_EMO:
-    text = text.replace(emot,' '.join(UNICODE_EMO[emot].replace(',','').replace(':','').replace('_',' ').split()))
-  return text
+    for emot in UNICODE_EMO:
+        text = text.replace(emot,' '.join(UNICODE_EMO[emot].replace(',', '').replace(':', '').replace('_', ' ').split()))
+    return text
 
-#preprocessing function
+
+# preprocessing function
 def preprocessing(reviews):
     processed_reviews = reviews.copy()
     for i in range(len(reviews)):
@@ -143,76 +151,83 @@ def preprocessing(reviews):
         processed_reviews[i] = remove_sp(processed_reviews[i])
         processed_reviews[i] = convert_emojis(processed_reviews[i])
         processed_reviews[i] = con(processed_reviews[i])
-    return(processed_reviews)
+    return (processed_reviews)
+
 
 st.title('SENTIMENTAL ANALYSIS ON REVIEWS')
 st.subheader("Enter the Product Name")
 name = st.text_input("")
 
-try : 
-  if st.button('Confirm') or name is not None:
-    dic = get_urls(name)
-    keys = list(dic.keys())
-    if len(keys)>=5:
-        for i in range(5):
-            st.write(keys[i])
-    
-        if st.checkbox('Show  some more Suggestions'):
+try:
+    if st.button('Confirm') or name is not None:
+        dic = get_urls(name)
+        keys = list(dic.keys())
+
+        if name == '':
+            print(keys[5]) #to induce index out of range error
+        if len(keys) >= 5:
+            for i in range(5):
+                st.write(keys[i])
+
+            if st.checkbox('Show  some more Suggestions'):
+                for i in range(len(keys)):
+                    if i >= 5:
+                        st.write(keys[i])
+        else:
             for i in range(len(keys)):
-                if i >= 5:
-                    st.write(keys[i])
-        else :
-        for i in range(len(keys)):
-            st.write(keys[i])  
-    try:
-        st.subheader("Copy the perfect name of the product of your interest from the above list")
-        new = st.text_input(" ")
-        if st.button('Yes my interest is true') or new != '':
-            reviews = get_reviews(dic, new)
-            
-            # preprocessing function
-            processed_reviews = preprocessing(reviews)
-            Analyze = SentimentIntensityAnalyzer()
-            compound1 = []
-            emoji = []
-            for x in range(len(processed_reviews)):
-                compound1.append(Analyze.polarity_scores(processed_reviews[x])['compound'])
+                st.write(keys[i])
+        try:
+            st.subheader("Copy the perfect name of the product of your interest from the above list")
+            new = st.text_input(" ")
+            if st.button('Yes my interest is true') or new != '':
+                reviews = get_reviews(dic, new)
 
-            # emoji's appending
-            for x in range(len(reviews)):
-                if compound1[x] >= 0.5:
-                    emoji.append('\U0001F600\U0001F600\U0001F600')
-                elif compound1[x] < 0.5 and compound1[x] > -0.5:
-                    emoji.append('\U0001F642\U0001F642')
-                elif compound1[x] <= -0.5:
-                    emoji.append('\U0001F611')
+                # preprocessing function
+                processed_reviews = preprocessing(reviews)
+                Analyze = SentimentIntensityAnalyzer()
+                compound1 = []
+                emoji = []
+                for x in range(len(processed_reviews)):
+                    compound1.append(Analyze.polarity_scores(processed_reviews[x])['compound'])
 
-            pd.options.display.max_colwidth = 100
-            df1 = pd.DataFrame({'Reviews': reviews,
-                                'Compound Score': compound1,
-                                'Emojis': emoji})
-            
-            col1, col2, col3 = st.beta_columns([1, 1, 1])
-            col2.image(dic[new][1], caption=new, use_column_width = True)
-    except KeyError:
-        st.write("Copy the Product name properly  \U0001F642")
+                # emoji's appending
+                for x in range(len(reviews)):
+                    if compound1[x] >= 0.5:
+                        emoji.append('\U0001F600\U0001F600\U0001F600')
+                    elif compound1[x] < 0.5 and compound1[x] > -0.5:
+                        emoji.append('\U0001F642\U0001F642')
+                    elif compound1[x] <= -0.5:
+                        emoji.append('\U0001F611')
+
+                pd.options.display.max_colwidth = 100
+                df1 = pd.DataFrame({'Reviews': reviews,
+                                    'Compound Score': compound1,
+                                    'Emojis': emoji})
+
+                col1, col2, col3 = st.beta_columns([1, 1, 1])
+                col2.image(dic[new][1], caption=new, use_column_width=True)
+        except KeyError:
+            st.write("Copy the Product name properly  \U0001F642")
 except IndexError:
     st.write("Please enter the product name \U0001F642 \U0001F642")
 
 print('Finished Processing\n')
 
 try:
-    if len(df) >=5 :
+    if len(df1) >= 5:
         col1, col2, col3 = st.beta_columns([0.1, 1, 0.01])
-        col2.dataframe(df1[0:5].style.set_properties(**{'text-align': 'center', 'color': 'black', 'background-color': 'white'}))
+        col2.dataframe(
+            df1[0:5].style.set_properties(**{'text-align': 'center', 'color': 'black', 'background-color': 'white'}))
 
         if st.checkbox('Show all reviews'):
             col1, col2, col3 = st.beta_columns([0.1, 1, 0.01])
-            if len(df1['Reviews']) >=5:
-                col2.dataframe(df1[5:].style.set_properties(**{'background-color': 'white', 'color': 'black', 'text-align': 'center'}))
-            else :
+            if len(df1['Reviews']) >= 5:
+                col2.dataframe(df1[5:].style.set_properties(
+                    **{'background-color': 'white', 'color': 'black', 'text-align': 'center'}))
+            else:
                 col2.write("No more reviews available")
     else:
-        col2.dataframe(df1.style.set_properties(**{'background-color': 'white', 'color': 'black', 'text-align': 'center'}))
+        col2.dataframe(
+            df1.style.set_properties(**{'background-color': 'white', 'color': 'black', 'text-align': 'center'}))
 except:
     pass
